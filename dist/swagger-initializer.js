7a17bfe99e2aef19fea3626a2a087ea2aa21d7e2
Jonathan García <jonfrgar@espol.edu.ec>
2022-11-20 14:16:18 -0500

    Added 2 def

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,9 +1,11 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
+  const definition = document.getElementById("initializer").getAttribute("definition");
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  console.log(definition)
   window.ui = SwaggerUIBundle({
-    url: "definition.yaml",
+    url: definition,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
